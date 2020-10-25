17323eb1734f083941c51f122f6982726cccbeb0
bslzz <bishalkoid@gmail.com>
2020-10-25 18:43:33 +0200

    db modified

--- db/initMongoose.js
+++ db/initMongoose.js
@@ -7,7 +7,6 @@ module.exports = mongoose.connect(process.env.MONGODB, {
 });
 
 const db = mongoose.connection;
-
 db.on('connected', () => console.log('Connected to DB successfully'));
 db.on('error', (err) => console.log(`Error connecting to DB : ${err}`));
 db.on('disconnected', () => console.log('DB disconnected'));
