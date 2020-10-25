61b72d958a31923827567f1f6de3415813f4fb8f
bslzz <bishalkoid@gmail.com>
2020-10-25 13:51:13 +0200

    initialized mongodb db

---
+++ db/initMongoose.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+module.exports = mongoose.connect(process.env.MONGODB, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('connected', () => console.log('Connected to DB successfully'));
+db.on('error', (err) => console.log(`Error connecting to DB : ${err}`));
+db.on('disconnected', () => console.log('DB disconnected'));
