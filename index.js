606f07335580fde36c264114486d6444b13a0bf9
bslzz <bishalkoid@gmail.com>
2020-10-25 13:00:56 +0200

    set up view engine for rendering ejs files

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ require('dotenv').config();
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+//middleware
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
 // starting server
 app.listen(PORT, () =>
   console.log(`Server running at http://localhost:${PORT}`)
