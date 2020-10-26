bb911ced238e6591fa2a6778d06b670ea25e114b
bslzz <bishalkoid@gmail.com>
2020-10-26 19:28:33 +0200

    modified codes to support deploying the project to heroku

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const express = require('express');
 require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 //import mongodb
 require('./db/initMongoose');
